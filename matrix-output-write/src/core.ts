cc2a040cc7ac56f06258b0d1f444927943402042
shiron <shiron4710.dev@gmail.com>
2025-01-17 14:00:41 +0900

    feat(matrix-output): impl matrix-output-write

--- matrix-output-write/src/core.ts
+++
@@ -1,8 +0,0 @@
-import type { InputOptions } from "@actions/core";
-
-export interface ActionsCore {
-  getInput: (name: string, options?: InputOptions) => string
-  debug: (message: string) => void
-  setFailed: (message: string | Error) => void
-  setOutput: (name: string, value: any) => void
-}
--- matrix-output-write/test/action.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, it } from "vitest";
-
-it("always passes", () => {
-  expect(true).toBe(true);
-});
